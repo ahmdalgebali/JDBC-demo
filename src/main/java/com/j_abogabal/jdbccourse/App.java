e88c1e29178955ddc7c71517c4af2fff86a21259
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-09-16 19:09:28 +0200

    add DB_file & DBConnection closes #1

---
+++ src/main/java/com/j_abogabal/jdbccourse/App.java
@@ -0,0 +1,13 @@
+package com.j_abogabal.jdbccourse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
